6ae475ac35b872599b636fe8409ce0b63213f738
shiva <shivamoeini2000@gmail.com>
2022-03-30 11:43:28 +0430

    cmit2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,47 @@
 const fill = document.querySelector(".fill");
 const empties = document.querySelectorAll(".empty");
-// fil Listeners
+
+// Fill listeners
 fill.addEventListener("dragstart", dragStart);
 fill.addEventListener("dragend", dragEnd);
-// drag functions
-function dragStart() {
-  //   console.log("start");
+
+// Loop through empty boxes and add listeners
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
 }
+
+// Drag Functions
+
+function dragStart() {
+  this.className += " hold";
+  setTimeout(() => (this.className = "invisible"), 0);
+}
+
 function dragEnd() {
-  //   console.log("End");
+  this.className = "fill";
+}
+
+function dragOver(e) {
+  //   console.log("over");
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  //   console.log("enter");
+  e.preventDefault();
+  this.className += " hovered";
+}
+
+function dragLeave() {
+  //   console.log("leves");
+  this.className = "empty";
+}
+
+function dragDrop() {
+  //   console.log("dragdrop");
+  this.className = "empty";
+  this.append(fill);
 }
