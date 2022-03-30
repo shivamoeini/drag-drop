e6dd93c100d356d0f58b9a9febc89ac8e1d03729
shiva <shivamoeini2000@gmail.com>
2022-03-30 11:11:56 +0430

    cmit1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+// fil Listeners
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+// drag functions
+function dragStart() {
+  //   console.log("start");
+}
+function dragEnd() {
+  //   console.log("End");
+}
